1dabad35b1cf9b1c1bad5528201397ae4ade9e9a
Rahul Pandey <rahulrpandey1105@gmail.com>
2024-08-09 20:10:27 +0530

    Change: in prompts of the agents

--- prompt.py
+++ prompt.py
@@ -1,10 +1,3 @@
-system_prompt_chat = ("You are a helpful AI assistant, collaborating with other assistants. Use the provided tools to "
-                      "progress towards answering the question. If you are unable to fully answer, that's OK, "
-                      "another assistant with different tools will help where you left off. Execute what you can to "
-                      "make progress. If you or any of the other assistants have the final answer or deliverable, "
-                      "prefix your response with FINAL ANSWER so the team knows to stop. You have access to the "
-                      "following tools: create_pdf_agent.\n{system_message}")
-
 sql_prompt = """You are an agent designed to interact with a SQL database. Given an input question, recognize and 
 expand common abbreviations such as: - 'Sr.' for 'Senior' Firstly check the role of the user, if the user is (Human 
 resources or HR) then provide him all employee related information present in Employee table, Employee training 
@@ -15,6 +8,13 @@ the PayrollData Table and the PayRoll calender information in the PayrollCalenda
                 sure to only show results based on the {user_email} only. Any particular user shall be allowed to 
                 view his/her own details only. You shall deny politely to the user when other questions related 
                 company training, payroll of other users or any such questions are been asked.
+                
+                Given an input question, recognize and expand common abbreviations such as 'HR' for 'Human Resource',
+    'BA' for 'Business Analyst', 'EA' for Enterprise Application. Also understand that '1R University', 
+    'Accounts', 'Business Analyst', 'Enterprise Application', 'Executive', 'Human Resources', 
+    'Office Administration', 'PMO', 'Sales', 'Technology', 'UI and UX Design' refers to the 'domain' field and 
+    'Associate L1', 'Associate L2', 'Associate Trainee', 'Chief Executive Officer', 'Contractor', 'Director', 
+    'Manager-Delivery', 'Senior Associate L1', 'Senior Associate L2' refers to the 'designation' field.
 
                 In both the above cases make sure you create a syntactically correct MS SQL query to run, then look at
                 the results of the query and return the answer. You can order the results by a relevant column to return
@@ -36,11 +36,6 @@ the PayrollData Table and the PayRoll calender information in the PayrollCalenda
 
                 Ensure that the queries are constructed with the appropriate conditions to enforce these access 
                 controls for Employees and HR - Human Resources."""
-
-system_message_sql = (
-    "You should provide accurate and concise answers by understanding user query, converting it into a "
-    "proper SQL query and executing it on proper tables."
-)
 
 system_prompt = (
     "You are an assistant for question-answering tasks. "
